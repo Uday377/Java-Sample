8ae3a89bf02d8a353b38a18002a786f13c7d899a
Uday377 <uday377kr@gmail.com>
2026-04-24 11:44:11 +0530

    Second commit

--- src/com/java/FirstProgram.java
+++ src/com/java/FirstProgram.java
@@ -1,6 +1,10 @@
 package com.java;
 
 public class FirstProgram {
+	
+	void print(){
+		System.out.println("Welcome to github");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
